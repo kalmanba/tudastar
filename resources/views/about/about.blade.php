02e5dc9aca87ea2a2de245ad52c1733dac4e5acc
kalmanba <155107591+kalmanba@users.noreply.github.com>
2023-12-29 20:35:47 +0100

    Added about section
    Needs to be extendended.

---
+++ resources/views/about/about.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-info-emphasis text-center pb-4">Rólunk</h1>
+        <!-- About section-->
+        <section id="about">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8 fs-5">
+                        <p>Ezen tudástár célja, hogy az itt felsorolt középiskolai tantárgyak elsajátításást segítse. Az itt található összefoglalók és segédletek összessége (remélhetőleg) tartalmazza az adott témakör elsajátításához szükséges anyagot. A kártyák segítségével válaszd ki azt a tantárgyat és évfolyamot ami érdekel, és nézz körül milyen anyagok állnak rendelkezésre.</p>
+                        <p class="fw-bold">Ha igazán segítségedre volt az, amit itt találtál kérlek fontold meg a tudástár
+                            <a class="text-decoration-none" target="_blank" href="https://buymeacoffee.com/hhtudastar"><span class="rainbow_text_animated">támogatását</span></a>.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
