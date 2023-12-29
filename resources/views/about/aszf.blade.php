6b4fb3b76261894edceb1d099825fab278242816
kalmanba <155107591+kalmanba@users.noreply.github.com>
2023-12-29 20:37:03 +0100

    Added EULA and copyright notice.
    Done, no further dev needed.

---
+++ resources/views/about/aszf.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-info-emphasis text-center pb-4">Általános Szerződési Feltételek</h1>
+        <!-- About section-->
+        <section id="about">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>1. Tartalom felhasználása:</h2>
+                        <p class="lead mb-4">A weboldalunkon található tartalmak, tananyagok és információk kizárólag információs célokat szolgálnak. Azok felhasználása kizárólag saját, nem kereskedelmi célra engedélyezett. A tartalmak másolása, terjesztése vagy módosítása tilos a szerzői jogok megsértése miatt.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>2. Pontosság és felelősség:</h2>
+                        <p class="lead mb-4">Bár mindent megteszünk annak érdekében, hogy a weboldalunkon található információk pontossága és frissessége magas szintű legyen, nem vállalunk felelősséget az esetleges pontatlanságokért vagy hibákért. A tartalom használata és értelmezése a felhasználó saját felelősségére történik.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>3. Harmadik felek tartalma és weboldalakra való hivatkozások:</h2>
+                        <p class="lead mb-4">Weboldalunkon található linkek és hivatkozások harmadik felek weboldalaira vezethetnek, melyeket mi nem ellenőrzünk. Nem vállalunk felelősséget ezeknek a weboldalaknak a tartalmáért vagy megbízhatóságáért. A külső weboldalak használata a felhasználó saját felelősségére történik.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>4. Jogvédő intézkedések:</h2>
+                        <p class="lead">Fenntartjuk a jogot, hogy bármikor módosítsuk vagy töröljük a weboldalunkon található tartalmat vagy a felhasználási feltételeket. Tilos bármilyen tevékenység, amely sérti a weboldalunkat, beleértve a jogi, etikai vagy technikai korlátozások megsértését.</p>
+                        <p class="lead mb-4 fw-bold">Felhasználóink számára fontos, hogy a weboldalunkat biztonságosan és zavartalanul használhassák. Kérjük, tartsa be ezeket a feltételeket a weboldalunkkal való interakció során.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
---
+++ resources/views/about/copyright.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-info-emphasis text-center pb-4">Szerzői jogok</h1>
+        <section id="about">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>A Tartalmak Jogai</h2>
+                        <p class="lead mb-4">Minden weboldalon található tanulmányi anyag, leírás, grafika, és egyéb tartalom saját készítésű vagy engedélyezett forrásokból származik, és jogvédelem alatt áll. Az összes jog, ideértve a szerzői jogokat, védjegyeket és egyéb szellemi tulajdonjogokat fenntartjuk. Az anyagaink másolása, terjesztése vagy felhasználása engedély nélkül szigorúan tilos.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>Miért Fontos Ez?</h2>
+                        <p class="lead mb-4">Az anyagaink létrehozása hosszú munkafolyamatot és kutatást igényelt. Azok az erőfeszítések, amelyeket az anyagaink elkészítésébe fektettünk, méltók a tiszteletünkre. Amikor másolják, terjesztik vagy jogtalanul használják az anyagainkat, súlyosan sértik munkánk és hozzájárulásunk értékét.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>Hogyan Segíthet?</h2>
+                        <p class="lead mb-4">Ön is részt vehet az anyagaink védelmében. Kérjük, ne másolja, ne terjessze és ne használja fel jogtalanul az itt található tartalmakat. Ha valamilyen formában szeretné felhasználni vagy megosztani az anyagainkat, kérjük, lépjen velünk kapcsolatba az engedélyezett felhasználás feltételeiről.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>Kapcsolatfelvétel</h2>
+                        <p class="lead">Amennyiben kérdése van az anyagaink jogi státuszával kapcsolatban vagy szeretné kérni a felhasználási engedélyt, kérjük, lépjen velünk kapcsolatba a következő e-mail címen: <strong class="fw-bold" >info@honaphire.net</strong>.</p>
+                        <p class="lead fw-bold mb-4"><strong>Köszönjük, hogy tiszteletben tartja a munkánkat és segít megőrizni az anyagaink integritását!</strong></p>
+                    </div>
+                </div>
+        </section>
+    </div>
+@endsection
