8c45ef7019bf49499110fdd0f06150f07b441c82
kalmanba <155107591+kalmanba@users.noreply.github.com>
2024-01-08 20:04:01 +0100

    added sitemap functionality

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 use App\Models\Study_guide;
-
+use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class DashController extends Controller
 {
@@ -33,6 +35,15 @@ class DashController extends Controller
 
         // Save the new record to the database
         $newStudyGuide->save();
+
+        $newguide = Study_guide::max('id');
+        SitemapGenerator::create('https://learn.honaphire.net')
+            ->getSitemap()
+            ->add(Url::create('/view-guide/' . $newguide)
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.8))
+            ->writeToDisk('public', 'sitemap.xml');
 
         return redirect('dash');
 
