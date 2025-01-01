452b2baaf441f3712a2b9da17d58d83ab3556927
kalmanba <155107591+kalmanba@users.noreply.github.com>
2025-01-01 21:42:00 +0100

    implemented advanced begging function

--- app/Http/Controllers/SubjectsController.php
+++ app/Http/Controllers/SubjectsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 use App\Models\Subject;
 use DateTime;
-
+use Illuminate\Support\Facades\File;
+use IntlDateFormatter;
 use Illuminate\Http\Request;
+use function PHPUnit\Framework\isFalse;
 
 class SubjectsController extends Controller
 {
@@ -19,7 +21,41 @@ class SubjectsController extends Controller
             session()->flash('showcd', true);
         }
 
+        $docxPath = '/fund.txt';
+        $fund =  File::get(storage_path($docxPath));
+
+        if ($fund < 50) {
+            $bgColor = "bg-danger";
+        }
+        elseif ($fund < 100) {
+            $bgColor = "bg-warning";
+        }
+        else {
+            $bgColor = "bg-success";
+        }
+        $fund = substr_replace($fund, '', -1);
+        $fund = $fund . "%";
+
+        // I don't care how stupid this looks, it works unlike the international time formatting thing.
+        $months = array(
+            "Január",
+            "Február",
+            "Március",
+            "Április",
+            "Május",
+            "Június",
+            "Július",
+            "Augusztus",
+            "Szeptember",
+            "Október",
+            "November",
+            "December"
+        );
+
+        $currentMonthNumber = date('n') - 1; // Subtracting 1 to match array index (0-based)
+        $month = $months[$currentMonthNumber];
+
         $subjects = Subject::all(); // Fetch all subjects from the database
-        return view('home', compact('subjects'));
+        return view('home', compact('subjects', 'bgColor', 'fund', 'month'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,10 @@
              <h1 class="text-info-emphasis">Üdv a tudástárban!</h1>
              <p>Ezen tudástár célja, hogy az itt felsorolt középiskolai tantárgyak elsajátításást segítse. Az itt található összefoglalók és segédletek összessége (remélhetőleg) tartalmazza az adott témakör elsajátításához szükséges anyagot. A kártyák segítségével válaszd ki azt a tantárgyat és évfolyamot ami érdekel, és nézz körül milyen anyagok állnak rendelkezésre.</p>
              <h2 class="text-danger mt-2">A tudsátár fennmaradása veszélyben!</h2>
-             <p class="fw-bold">A tudástár szerereinek költségeit az eseti támogatások nem fedezik. Ha igazán segítségedre volt az, amit itt találtál kérlek fontold meg a tudástár
+             <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                 <div class="progress-bar progress-bar-striped progress-bar-animated {{ $bgColor }}" style="width: {{ $fund }}"></div>
+             </div>
+             <p class="fw-bold">A {{ $month }} havi költségeinknek {{ $fund }}-a fedezett. Ha igazán segítségedre volt az, amit itt találtál kérlek fontold meg a tudástár
                  <a class="text-decoration-none fw-bold" target="_blank" href="/donate"><span class="rainbow_text_animated">rendszeres támogatását</span></a>!</p>
          </div>
      </div>
