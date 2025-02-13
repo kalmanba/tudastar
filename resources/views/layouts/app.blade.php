7dca0d780ec0df77a10e14396fffb17c7bee08de
kalmanba <155107591+kalmanba@users.noreply.github.com>
2025-02-13 17:30:13 +0100

    new font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Space+Grotesk|Space+Grotesk:bold">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     <!-- Css -->
     <link rel="stylesheet" href="/css/landing.css">
