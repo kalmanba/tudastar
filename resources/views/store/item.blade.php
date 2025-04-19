2894e81280dd4fbf501e29ddafcae7f424c980b5
kalmanba <marton@kurthy.org>
2025-04-19 17:37:27 +0200

    fixed js issues

--- resources/views/store/item.blade.php
+++ resources/views/store/item.blade.php
@@ -74,7 +74,7 @@
     </div>
 </div>
 
-<script src="/js/cart.js"></script>
+<script type="module" src="/js/cart.js"></script>
 
 <!-- Floating Cart Button -->
 <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 1000;">
