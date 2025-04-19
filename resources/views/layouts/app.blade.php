bdd28a23101385ae01e3f5a9e8c0d12eea17592f
kalmanba <marton@kurthy.org>
2025-04-19 21:53:45 +0200

    fixed css and nodejs issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,7 @@
     <link rel="stylesheet" href="/css/landing.css">
     <link rel="stylesheet" href="/css/rainbow.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
