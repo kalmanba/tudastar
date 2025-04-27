8cbe0a3e59d13edc8b2cbd97e269a73213056979
kalmanba <marton@kurthy.org>
2025-04-27 10:38:59 +0200

    revamped navigation, added & implemented slug

--- resources/views/app/gradeselector.blade.php
+++ resources/views/app/gradeselector.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <h1 class="display-6 text-info mb-1 text-dark"><a class="backspan fs-2" href="/"><i class="backspan bi bi-arrow-90deg-up"></i></a> {{ $subjectname }}</h1>
+        <h1 class="display-6 text-info mb-1 text-dark"><a class="backspan fs-2" href="/"><i class="backspan bi bi-arrow-90deg-up"></i></a> {{ $subject->name }}</h1>
         <h2 class="text-subtitle">Évfolyamválasztó:</h2>
     @foreach($grades as $grade)
             <div class="col-sm-6 col-lg-3 mb-4 mb-lg-0">
@@ -13,12 +13,7 @@
                     <h3 class="h6 mb-3 card-title lh-base fw-bold">{{ $grade->grade }}. osztály</h3>
                     <div class="row align-items-center">
                         <div class="order-2 order-lg-1 col-lg-6">
-                            <form method="GET" action="/list-guides">
-                                @csrf
-                                <input name="subject_id" type="hidden" value="{{ $subjectid }}">
-                                <input name="grade_id" type="hidden" value="{{ $grade->id }}">
-                                <button class="button-bg btn btn-info">Megnézem</button>
-                            </form>
+                            <a href="/{{ $subject->slug }}/{{ $grade->slug }}"><button class="button-bg btn btn-info">Megnézem</button></a>
                         </div>
                     </div>
                 </div>
